d50d29348e6ed9193da71b007df770ccd7963172
Nurbolat <nurba.dav@gmail.com>
2022-07-15 19:18:39 +0600

    init: initial commit

---
+++ html/src/n2749/tetris/client/HtmlLauncher.java
@@ -0,0 +1,22 @@
+package n2749.tetris.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import n2749.tetris.Tetris;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                // Resizable application, uses available space in browser
+                return new GwtApplicationConfiguration(true);
+                // Fixed size application:
+                //return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new Tetris();
+        }
+}
